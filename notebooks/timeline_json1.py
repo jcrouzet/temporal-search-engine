bd719c77161af9d4c00fcc9945ea8c7c6b3b3869
Haroke <kevinpasini@hotmail.fr>
2016-11-09 13:57:18 +0100

    Update timeline_json1.py

--- notebooks/timeline_json1.py
+++ notebooks/timeline_json1.py
@@ -1,34 +1,34 @@
 import numpy as np
+import json
 
 from query import *
 
-def timeline_json(events, dates):
+def timeline_json(events, dates, query):
     """
     """
     tl_json = { "events": [] }
-	num=0;
+    num=0
     for event in events:
-		num+=1;
-        d_start = dates[event[0]]
-        d_end = dates[event[1]]
-
-        tl_json['events'].append(tmp)
+	num+=1
+        d_start = dates[event[0]-1]
+        d_end = dates[event[1]-1]
 
         # Ajout de l'article marquant de l'intervalle
         tmp = {}
-        res_req = query_articles("séisme", d_start, d_end)
+        res_req = query_articles(query, d_start, d_end)
 
         tmp["start"] = res_req["hits"]["hits"][0]["_source"]["date_art"].encode('ascii','ignore')
         tmp["title"] = num
-        tmp["description"] = "Score de : " + str(res_req["hits"]["hits"][0]["_score"]) + "/n" + res_req["hits"]["hits"][0]["_source"]["id_art"].encode('ascii','ignore')
+        tmp["description"] = "Score de : " + str(res_req["hits"]["hits"][0]["_score"])+ "/n" + res_req["hits"]["hits"][0]["_source"]["id_art"].encode('ascii','ignore')
         tmp["link"] = "http://localhost:5601/app/kibana#/doc/spliine/spliine/article?id=" + res_req["hits"]["hits"][0]["_source"]["id_art"].encode('ascii','ignore')
 
+        tl_json['events'].append(tmp)
 
         # Ajout de l'intervalle
         tmp = {}
         tmp["start"] = d_start
         tmp["end"] = d_end
-        tmp["title"] =  ""
+        tmp["title"] = ""
         tmp["durationEvent"] = 'true'
         tmp["description"] = "Evenement entre " + d_start + " et " + d_end
         tmp["link"] = ""
