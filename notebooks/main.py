948ba98254b71933a3a0c8b4111a342308087de3
mathemare <mathemare@hotmail.fr>
2016-11-09 13:43:57 +0100

    Debugged version of main, query and timeline_json script

--- notebooks/main.py
+++ notebooks/main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from query import *
 from events_detection import *
-from timeline_json import * 
+from timeline_json import *
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -18,21 +18,21 @@ def main(argv=None):
     if len(argv) != 2:
         print >>sys.stderr, "usage: " + argv[0] + " <query>"
         return 2
-    
+
     hist, dates = query_hist(argv[1])
-    
-    # Peak detection 
+
+    # Peak detection
     lag = 60
     thresh = 3.5
     influence = 0.01
-    
+
     sig, avg, std = peaks_detection(hist, lag, thresh, influence)
-    
-    # Events 
+
+    # Events
     events = events_list(sig)
     
     # Timeline creation
-    timeline_json(events, dates)
+    timeline_json(events, dates, argv[1])
 
 if __name__ == "__main__":
     sys.exit(main())
--- notebooks/query.py
+++ notebooks/query.py
@@ -9,9 +9,7 @@ def query_hist(query):
                 doc_type="article",
                 body='{"query":{"match":{"article":"'+ query +'"}},"size":0,"aggregations":{"date_hist":{"date_histogram":{"field":"date_art","interval":"day", "format" : "yyyy-MM-dd"}}}}')
 
-    print(query + " : %d articles corerespondants" % res_dimanche['hits']['total'])
 
-    res_to_hist= {}
     results = res['aggregations']['date_hist']['buckets']
     counts = []
     dates = []
@@ -20,8 +18,7 @@ def query_hist(query):
         #print("%s) %s" % (doc['key_as_string'], doc['doc_count']))
         counts.append(doc['doc_count'])
         dates.append(doc['key_as_string'].encode('ascii','ignore'))
-        res_to_hist[doc['key_as_string'].encode('ascii','ignore')] = doc['doc_count']
-        
+
     return((counts, dates))
 
 def query_articles(query, start, end):
@@ -30,5 +27,5 @@ def query_articles(query, start, end):
     es = Elasticsearch()
     res = es.search(index="spliine",
                 doc_type="article",
-                body='{"query":{"bool":{"must":{"range":{"date_art":{"gte":"'+ date1 +'","lte":"'+ date2 +'"}}},"filter":{"query_string":{"query":"'+ query +'","default_operator":"AND"}}}},"size":5}')
+                body='{"query":{"bool":{"must":{"range":{"date_art":{"gte":"'+ start +'","lte":"'+ end +'"}}},"filter":{"query_string":{"query":"'+ query +'","default_operator":"AND"}}}},"size":5}')
     return res
